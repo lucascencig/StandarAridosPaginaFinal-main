4bab43395a1f567b6dde590cbdbf85bd99538dc0
LucasCencig <74467300+lucascencig@users.noreply.github.com>
2021-11-26 23:34:11 -0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,69 @@
+window.sr = ScrollReveal();
+
+//INDEX
+
+sr.reveal('.grid-eleccion', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+})
+
+sr.reveal('.galeria', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+})
+
+sr.reveal('.nosotros', {
+    duration: 2000,
+    origin: 'rigth',
+    distance: '100px'
+})
+
+
+//REVESTIMIENTOS TEXTURADOS
+
+sr.reveal('.animado', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+})
+
+
+
+//LATEX COLOR:
+
+sr.reveal('.animado', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+})
+
+
+//PREPARACION DE LA SUPERFICIE:
+
+sr.reveal('.listaProductos', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+})
+
+
+//AUXILIARES:
+
+sr.reveal('.tarjeta', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+})
+
+
+//MEMBRANAS:
+
+sr.reveal('.contenedorTarjetas', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+})
+
+
